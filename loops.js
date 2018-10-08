47fc324c9e78953f5fc4981668bd2b9f78fc2e1c
Daniel Hamilton <danhamilton16@gmail.com>
2018-10-08 21:08:57 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,16 @@
+function forLoop(arr){
+  for(let i=1; i<25; i++){
+    if (i==1){
+      arr.push("I am 1 strange loop.")
+    }else{
+      arr.push(`I am ${i} strange loops`)
+    }
+  }
+}
+
+function whileLoop(int){
+  while(int>0){
+    int=int-1;
+  }
+  return "done";
+}
